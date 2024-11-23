59064a5e7e93c1e017faeebfd8056d0f0d551bc5
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2024-11-23 16:24:20 +0100

    added nginx location /api/

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,10 +29,17 @@ function About() {
 }
 
 function Contact() {
+  const handleClick = async () => {
+    const response = await fetch('/api/');
+    console.log('==================');
+    console.log('response', await response.json());
+    console.log('==================');
+  }
   return (
     <div>
       <h1>Contact Page</h1>
       <p>Feel free to reach out to us here!</p>
+      <button onClick={handleClick}>fetch</button>
     </div>
   );
 }
