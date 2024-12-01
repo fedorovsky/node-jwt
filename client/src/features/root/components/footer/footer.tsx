06872a8af05bf6904c0ec56f42208c8d2585d35e
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2024-12-01 09:46:36 +0100

    added hash

--- client/src/features/root/components/footer/footer.tsx
+++ client/src/features/root/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 export const Footer = () => {
 	return (
 		<div className="container mx-auto text-center">
+			<p className="text-sm">HASH: {import.meta.env.VITE_GIT_COMMIT_HASH}</p>
 			<p className="text-sm">
 				© {new Date().getFullYear()} My Website. All rights reserved.
 			</p>
